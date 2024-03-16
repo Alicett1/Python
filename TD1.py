c2344501a66091d73a0968c91d9b02353cb27070
unknown <alice.toutain7@etu.univ-lorraine.fr>
2024-03-16 15:45:41 +0100

    ”Python”

--- TD1.py
+++ TD1.py
@@ -32,13 +32,17 @@ for k in nbre:
         maxi=k
 print(maxi)
 
-def score(mot):
-    moins=[a,e,i,l,n,o,r,s,t,u]
-    deux=[d,g,m]
-    trois=[b,c,p]
-    quatre=[f,h,v]
-    huit=[j,q]
-    dix=[k,w,x,y,z]
+
+#Pour représenter les points associés aux lettres, on utilise des listes.
+
+def scoredf(mot):
+    score=0
+    moins=['a','e','i','l','n','o','r','s','t','u']
+    deux=['d','g','m']
+    trois=['b','c','p']
+    quatre=['f','h','v']
+    huit=['j','q']
+    dix=['k','w','x','y','z']
     
     for i in mot:
         if i in moins:
@@ -53,16 +57,67 @@ def score(mot):
             score=score+8
         if i in dix:
             score=score+10
+    return score
             
 def max_score(listemots):
-    scoretotal=[]
-    for i in listemots:
-        scoretotal.append(score(i))
+    scoretotal=[0 for i in range(len(listemots))]
+    for i in range(len(listemots)):
+        scoretotal[i]=scoretotal[i]+scoredf(listemots[i])
+        
     maxi2=scoretotal[0]
-    for k in scoretotal:
-        if len(k)>len(maxi2):
-            maxi2=k
-    return maxi2
-        
-        
-      
+    p=0
+    for k in range(len(scoretotal)):
+        if scoretotal[k]> maxi2:
+            p=listemots[k]
+            maxi2= scoretotal[k]
+    return maxi2, p
+
+
+#%%Exercice 4
+#Il faut ajouter une liste de 0 points avec le symbole '?'. Et ajouter une condition qui fait que lorsque i parcourt les lettres d'un mot, s'il trouve plus d'un joker, le joueur perd. 
+
+def scoredf(mot):
+    score=0
+    moins=['a','e','i','l','n','o','r','s','t','u']
+    deux=['d','g','m']
+    trois=['b','c','p']
+    quatre=['f','h','v']
+    huit=['j','q']
+    dix=['k','w','x','y','z']
+    zero=['?']
+    
+    for i in mot:
+        b=0
+        if i in moins:
+            score=score+1
+        if i in deux:
+            score=score+2
+        if i in trois:
+            score=score+3
+        if i in quatre:
+            score=score+4
+        if i in huit:
+            score=score+8
+        if i in dix:
+            score=score+10
+        if i in zero:
+            b=b+1
+            score=score
+    return score
+            
+def max_score(listemots):
+    nbre_interrogation=0
+    scoretotal=[0 for i in range(len(listemots))]
+    for i in range(len(listemots)):
+        scoretotal[i]=scoretotal[i]+scoredf(listemots[i])
+        nbre_interrogation=nbre_interrogation+b
+    if nbre_interrogation>1:
+        return False
+    maxi2=scoretotal[0]
+    p=0
+    for k in range(len(scoretotal)):
+        if scoretotal[k]> maxi2:
+            p=listemots[k]
+            maxi2= scoretotal[k]
+    return maxi2, p
+
